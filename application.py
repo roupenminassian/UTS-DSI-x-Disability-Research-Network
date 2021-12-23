a8282e32c51fdd31cd37209e8ae83b649a65751b
Roupen Minassian <61346944+roupenminassian@users.noreply.github.com>
2021-12-23 13:11:48 +1100

    Update application.py

--- application.py
+++ application.py
@@ -28,7 +28,7 @@ if input is None:
         pass
 
 else:
-    with open(input,"rb") as fp:# Unpickling
+    with open(str(uploaded_file.name) + '.txt',"rb") as fp:# Unpickling
         contents = pickle.load(fp)
   
     #Preparing model
